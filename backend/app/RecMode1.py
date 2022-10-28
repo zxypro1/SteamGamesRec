dab404966efd4e2d41950428ebba4815eed7f06d
XindiSindy <386760499@qq.com>
2022-10-28 17:00:49 +0800

    Update RecMode1.py

--- backend/app/RecMode1.py
+++ backend/app/RecMode1.py
@@ -7,22 +7,41 @@ from .main import resources
 
 bp = Blueprint("RecMode1", __name__)
 
-@bp.route("/chooseTag", methods=['POST'])
-def getTags():
+# @bp.route("/chooseTag", methods=['POST'])
+# def getTags():
+#     if request.method == 'POST':
+#         db = get_db()
+#         data_json = json.loads(request.get_data())
+#         tags = list()
+#         for data in data_json:
+#             for key, value in data.items():
+#                 tags.append(value)
+#         return gameDataAccess.getGameInfoByGenre(tags)
+#     else:
+#         abort(404, "Error")
+
+@bp.route("/allGameInfo")
+def allGameInfo():
+    return gameDataAccess.getAllGameInfo()
+
+
+
+@bp.route("/getSimilarGamesByItem", methods=['POST'])
+def getSimilarGamesByItem():
     if request.method == 'POST':
-        db = get_db()
-        data_json = json.loads(request.get_data())
-        tags = list()
-        for data in data_json:
-            for key, value in data.items():
-                tags.append(value)
-        return gameDataAccess.getGameInfoByGenre(tags)
+        item_id = request.values.get('gameId')
+        return gameDataAccess.getRecByItem(item_id)
     else:
         abort(404, "Error")
 
-@bp.route("/getSimilarGames")
-def getSimilarGames(gameId):
+@bp.route("/getSimilarGamesByUser", methods=['POST'])
+def getSimilarGamesByUser():
+    if request.method == 'POST':
+        user_id = request.values.get('userId')
+        return gameDataAccess.getRecByUser(user_id)
 
+    else:
+        abort(404, "Error")
 
 
 
