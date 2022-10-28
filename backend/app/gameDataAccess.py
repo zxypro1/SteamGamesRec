79446248a58c8f4819bc0b4639b37cc04ac846aa
XindiSindy <386760499@qq.com>
2022-10-28 17:00:45 +0800

    Update gameDataAccess.py

--- backend/app/gameDataAccess.py
+++ backend/app/gameDataAccess.py
@@ -1,5 +1,29 @@
 from .main.db import get_db
-from flask import url_for
+from flask import url_for, request, g
+import pandas as pd
+from .main import resources
+
+
+
+def getAllGameInfo():
+    db = get_db()
+    result = db.execute('SELECT * FROM gamesdws')
+    result = result.fetchall()
+    return result
+
+def getRecByItem(item_id):
+    item_embedding_matrix = pd.read_csv(request.files['embeddings_item.csv'])
+    return resources.get_item_recs(item_embedding_matrix,item_id,g.item_dict,100,True)
+
+def getRecByUser(user_id):
+
+    user_dict = resources.create_user_dict(g.interactions)
+    scores = resources.get_recs(g.model,user_id,user_dict,g.item_dict,0,100,True,True)
+    return getGameInfoByName(scores)
+
+
+
+
 
 def getGameInfoById(idArr):
     db = get_db()
@@ -12,7 +36,16 @@ def getGameInfoById(idArr):
         retVal.append(dict(i))
     return retVal
 
-
+def getGameInfoByName(nameArr):
+    db = get_db()
+    nameArr = list(nameArr)
+    result = db.execute('SELECT * FROM GameDetail WHERE gameName IN (%s)' %
+                        ','.join('?'*len(nameArr)), nameArr)
+    result = result.fetchall()
+    retVal = list()
+    for i in result:
+        retVal.append(dict(i))
+    return retVal
 
 def getGameInfoByGenre(genreArr):
     db = get_db()
