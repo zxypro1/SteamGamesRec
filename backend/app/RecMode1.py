7f6856730b41a1494f324bd118395233b96dae88
XindiSindy <386760499@qq.com>
2022-10-28 11:38:52 +0800

    Update RecMode1.py

--- backend/app/RecMode1.py
+++ backend/app/RecMode1.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, request, make_response
 import json
+from werkzeug.exceptions import abort
 from .main.db import get_db
+from . import gameDataAccess
+from .main import resources
 
 bp = Blueprint("RecMode1", __name__)
 
@@ -13,13 +16,12 @@ def getTags():
         for data in data_json:
             for key, value in data.items():
                 tags.append(value)
-        result = db.execute('SELECT * FROM GameDetail WHERE genra IN (%s)' %
-                            ','.join(['%s']*len(tags)))
-        result = result.fetchall()
-        retVal = list()
-        for i in result:
-            retVal.append(dict(i))
-        return retVal
+        return gameDataAccess.getGameInfoByGenre(tags)
+    else:
+        abort(404, "Error")
+
+@bp.route("/getSimilarGames")
+def getSimilarGames(gameId):
 
 
 
