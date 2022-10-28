89d7bf90b6f0f5329427fb6100a477ba498e6f4c
XindiSindy <386760499@qq.com>
2022-10-28 17:00:56 +0800

    Update hello.py

--- backend/hello.py
+++ backend/hello.py
@@ -1,7 +1,19 @@
 from app import creat_app
-
+from flask import g, request
+import pandas as pd
+import pickle
 
 app = creat_app()
+
+@app.before_first_request
+def first_action():
+    g.item_dict = pd.read_csv(request.files['gamelist.csv'])
+    g.interactions = pd.read_csv(request.files['interactions.csv'])
+    res = open('savemodel.pickle', 'rb')
+    g.model = pickle.load(res)
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
